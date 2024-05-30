a6eb85138222c960b546191f0cbc376502323bcb
Ehsan Nasiri <ehsann@google.com>
2024-05-30 14:13:01 -0700

    remove dead code.

--- index.ts
+++ index.ts
@@ -14,18 +14,6 @@
  * limitations under the License.
  */
 
-// import {
-//   Firestore,
-//   Filter,
-//   DocumentData,
-//   setLogFunction,
-//   Settings,
-//   CollectionReference,
-//   DocumentReference,
-//   Query,
-//   QuerySnapshot,
-//   FieldValue
-// } from '@google-cloud/firestore';
 
 import { trace } from "@opentelemetry/api";
 
@@ -76,108 +64,6 @@ registerInstrumentations({
 
 console.log("Done initializing Tracing...");
 
-// This is the main application function. We wrap this program in an asynchronous function to
-// avoid errors with top-level awaits.
-// async function main(): Promise<void> {
-//   // Enable this block to log SDK messages
-//   setLogFunction((msg: string) => {
-//     console.log(`LogFunction: ${msg}`);
-//   });
-//   const settings: Settings = {
-//     projectId: 'myproject-d5314',
-//     preferRest: true,
-//     openTelemetryOptions: {
-//       enableTracing: true,
-//       traceProvider: trace.getTracerProvider()
-//     }
-//   };
-//
-//   // Initialize Firestore
-//   const firestore = new Firestore(settings);
-//
-//   await firestore.collection('foo').doc("bar").get();
-//
-//   /*
-//   // Create a collection reference to collection "foo"
-//   let myCollection: CollectionReference = firestore.collection('foo');
-//
-//   // Add a document to the collection
-//   let myDocA: DocumentReference<DocumentData> = await myCollection.add({
-//     myField: true
-//   });
-//
-//   // Set a document (alternative to add) in the collection
-//   let myDocB: DocumentReference<DocumentData> = firestore.doc('foo/B');
-//   // the second argument "options" is not required, defaults to merge false
-//   await myDocB.set({ myField: false }, { merge: false });
-//
-//   // Read a document
-//   let readDocA = await myDocA.get();
-//   */
-//
-//   /*
-//   // Access the data in a specific field
-//   console.log(`readDocA.get("myField") --> ${readDocA.get('myField')}`);
-//
-//   // Access data in all fields using `documentReference.data()`
-//   let documentData: any = readDocA.data();
-//   console.log(`readDocA.data() --> ${JSON.stringify(documentData)}`);
-//
-//   // Create a query that filters documents
-//   let myQuery: Query<DocumentData> = myCollection.where('myField', '==', true);
-//   let queryResults: QuerySnapshot = await myQuery.get();
-//   if (!queryResults.empty) {
-//     let firstDoc = queryResults.docs[0];
-//     // ... do something with firstDoc
-//   }
-//   */
-//
-//   /*
-//   // Perform an OR query
-//   myQuery = myCollection.where(
-//     Filter.or(
-//       Filter.where('myField', '==', true),
-//       Filter.where('myField', '<', 0)
-//     )
-//   );
-//
-//   // Update a document
-//   await myDocB.update({
-//     myField: true,
-//     updateTime: FieldValue.serverTimestamp()
-//   });
-//   */
-//
-//   /*
-//   const tracer = trace.getTracer("foo");
-//   // const span1 = tracer.startSpan("span1");
-//   // for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {}
-//   // span1.end();
-//   // const span2 = tracer.startSpan("span2");
-//   // for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {}
-//   // span2.end();
-//   // const span3 = tracer.startSpan("span3");
-//   // for (let i = 0; i <= Math.floor(Math.random() * 40000000); i += 1) {}
-//   // span3.end();
-//
-//   await tracer.startActiveSpan("main", async span1 => {
-//     await tracer.startActiveSpan("read docA", async (span2) => {
-//       await myDocA.get();
-//       await tracer.startActiveSpan("read docB", async (span3) => {
-//         await myDocB.get();
-//         span3.end();
-//       });
-//       span2.end();
-//     });
-//     span1.end();
-//   });
-//   await provider.shutdown();
-//   */
-//   await provider.forceFlush();
-//   await new Promise(resolve => setTimeout(resolve, 1000));
-//   await firestore.terminate();
-// }
-
 function makeHttpGetRequest() {
   const options = {
     host: 'www.google.com',
@@ -204,4 +90,3 @@ function makeHttpGetRequest() {
 }
 
 makeHttpGetRequest();
-//main();
